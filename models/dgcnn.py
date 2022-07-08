0daaac42812a72d3facdf4d63d378b03c62274f0
BarryHu <s56hu@uwaterloo.ca>
2022-07-08 15:32:16 +0200

    add dgcnn_fps

--- models/dgcnn.py
+++ models/dgcnn.py
@@ -3,8 +3,114 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.utils.data
 import torch.nn.functional as F
+from knn_cuda import KNN
+from pointnet2_ops import pointnet2_utils
 from models.utils.transform_net import Transform_Net
 from models.utils.dgcnn_util import get_graph_feature
+knn = KNN(k=16, transpose_mode=False)
+
+class DGCNN_downsize(nn.Module):
+    def __init__(self):
+        super().__init__()
+        '''
+        K has to be 16
+        '''
+        self.input_trans = nn.Conv1d(3, 8, 1)
+
+        self.layer1 = nn.Sequential(nn.Conv2d(16, 32, kernel_size=1, bias=False),
+                                   nn.GroupNorm(4, 32),
+                                   nn.LeakyReLU(negative_slope=0.2)
+                                   )
+
+        self.layer2 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1, bias=False),
+                                   nn.GroupNorm(4, 64),
+                                   nn.LeakyReLU(negative_slope=0.2)
+                                   )
+
+        self.layer3 = nn.Sequential(nn.Conv2d(128, 256, kernel_size=1, bias=False),
+                                   nn.GroupNorm(4, 256),
+                                   nn.LeakyReLU(negative_slope=0.2)
+                                   )
+
+        self.layer4 = nn.Sequential(nn.Conv2d(512, 1024, kernel_size=1, bias=False),
+                                   nn.GroupNorm(4, 1024),
+                                   nn.LeakyReLU(negative_slope=0.2)
+                                   )
+
+    
+    @staticmethod
+    def fps_downsample(coor, x, num_group):
+        xyz = coor.transpose(1, 2).contiguous() # b, n, 3
+        fps_idx = pointnet2_utils.furthest_point_sample(xyz, num_group)
+
+        combined_x = torch.cat([coor, x], dim=1)
+
+        new_combined_x = (
+            pointnet2_utils.gather_operation(
+                combined_x, fps_idx
+            )
+        )
+
+        new_coor = new_combined_x[:, :3]
+        new_x = new_combined_x[:, 3:]
+
+        return new_coor, new_x
+
+    @staticmethod
+    def get_graph_feature(coor_q, x_q, coor_k, x_k):
+
+        # coor: bs, 3, np, x: bs, c, np
+
+        k = 16
+        batch_size = x_k.size(0)
+        num_points_k = x_k.size(2)
+        num_points_q = x_q.size(2)
+
+        with torch.no_grad():
+            _, idx = knn(coor_k, coor_q)  # bs k np
+            assert idx.shape[1] == k
+            idx_base = torch.arange(0, batch_size, device=x_q.device).view(-1, 1, 1) * num_points_k
+            idx = idx + idx_base
+            idx = idx.view(-1)
+        num_dims = x_k.size(1)
+        
+        x_k = x_k.transpose(2, 1).contiguous()
+        feature = x_k.view(batch_size * num_points_k, -1)[idx, :]
+        feature = feature.view(batch_size, k, num_points_q, num_dims).permute(0, 3, 2, 1).contiguous()
+        x_q = x_q.view(batch_size, num_dims, num_points_q, 1).expand(-1, -1, -1, k)
+        feature = torch.cat((feature - x_q, x_q), dim=1)
+        return feature
+
+    def forward(self, x):
+
+        # x: bs, 3, np
+
+        # bs 3 N(128)   bs C(224)128 N(128)
+        coor = x
+        f = self.input_trans(x)
+
+        f = self.get_graph_feature(coor, f, coor, f)
+        f = self.layer1(f)
+        f = f.max(dim=-1, keepdim=False)[0]
+
+        coor_q, f_q = self.fps_downsample(coor, f, 512) #[4,3,512], [4,32,512]
+        f = self.get_graph_feature(coor_q, f_q, coor, f) #[4,64,512,16]
+        f = self.layer2(f) #[4,64,512,16]
+        f = f.max(dim=-1, keepdim=False)[0] #[4,64,512]
+        coor = coor_q #[4,3,512]
+
+        f = self.get_graph_feature(coor, f, coor, f) #[4,128,512,16]
+        f = self.layer3(f) #[4,64,512,16]
+        f = f.max(dim=-1, keepdim=False)[0] #[4,64,512]
+
+        coor_q, f_q = self.fps_downsample(coor, f, 128) #[4,3,128], [4,64,128]
+        f = self.get_graph_feature(coor_q, f_q, coor, f) #[4,128,128,16]
+        f = self.layer4(f) #[4,128,128,16]
+        f = f.max(dim=-1, keepdim=False)[0] #[4,128,128]
+        coor = coor_q #[4,3,128]
+
+        return coor, f
+
 
 class DGCNN(nn.Module):
     def __init__(self, args, latent_dim=1024, grid_size=4, only_coarse=False):
@@ -23,10 +129,6 @@ class DGCNN(nn.Module):
         self.bn4 = nn.BatchNorm2d(64)
         self.bn5 = nn.BatchNorm2d(64)
         self.bn6 = nn.BatchNorm1d(1024)
-        self.bn7 = nn.BatchNorm1d(64)
-        self.bn8 = nn.BatchNorm1d(256)
-        self.bn9 = nn.BatchNorm1d(256)
-        self.bn10 = nn.BatchNorm1d(128)
 
         self.conv1 = nn.Sequential(nn.Conv2d(6, 64, kernel_size=1, bias=False),
                                    self.bn1,
