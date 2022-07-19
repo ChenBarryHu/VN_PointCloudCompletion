2311c1411d20e9290f205ca943ba6a41984c7f85
BarryHu <s56hu@uwaterloo.ca>
2022-07-19 18:32:54 +0200

    update test logic

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-python main.py -n 07-12_16384_points_dgcnn_fps_vn_foldingnet_with_seed_001 --resume test
+python main.py -n 07-16_16384_points_dgcnn_fps_foldingnet_with_seed_003 --resume test
